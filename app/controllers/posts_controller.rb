352dbd61b14e7826b5c5b4fa3cf9617a1aa38535
An Nuyen <annuyen@gmail.com>
2018-03-08 03:03:57 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,4 +18,10 @@ class PostsController < ApplicationController
 	  @post.save
 	  redirect_to post_path(@post)
 	end
+
+	get 'posts/:id/edit', to: 'posts#edit',
+	as: :edit_post 
+
+	put 'posts/:id', to: 'posts#update'
+	
 end
